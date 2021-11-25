2bde6a74a4789e7c3122aea2e27cfb1c0d47517f
Renan <renan@cso.com.br>
2021-11-25 18:45:57 -0300

    Revisão parâmetros

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -19,19 +19,19 @@ const float kl = 1.726e-08; //(N*s^2) Constante de sustentação (lift constant)
 const float kd = 8.939e-10; //(N*m*s^2) Constante de arrasto (drag constant)
 
 //parâmetros do filtro passa-baixas do estimador de atitude
-const float wc = 0.1; //frequência de corte (rad/s)
+const float wc = 1.0; //frequência de corte (rad/s)
 const float dt = 0.002; //tempo de amostragem (2ms)
 const float alpha = (wc*dt)/(1+(wc*dt)); //fator de suavização
 
 //Ganhos dos controladores de atitude 
 // Os ganhos de roll e pitch (phi e theta) devem ser iguais pela simetria do drone
-//%OS = 1% e Ts = 0.3s 
-const float kp_rp = 260.51; //ganho proporcional de roll e pitch (phi e theta)
+//%OS = 0.5% e Ts = 0.3s 
+const float kp_rp = 240.28; //ganho proporcional de roll e pitch (phi e theta)
 const float kd_rp = 26.67; //ganho derivativo de roll e pitch (phi e theta)
 
-//%OS = 0.1% e Ts = 0.5s
-const float kp_psi = 93.8; //ganho proporcional de yaw (psi)
-const float kd_psi = 16.0; //ganho derivativo de yaw (psi)
+//%OS = 0.5% e Ts = 0.6s
+const float kp_psi = 60.07; //ganho proporcional de yaw (psi)
+const float kd_psi = 13.33; //ganho derivativo de yaw (psi)
 
 //parâmetros para o estimador vertical (altitude)
 const float dt_range = 0.05; //tempo de amostragem do sensor de range (20ms)
@@ -44,14 +44,14 @@ const float l1 = wc_range*wc_range; //ganho do estimador
 const float l2 =2*zeta*wc_range;
 
 //Ganhos do controlador vertical
-//%OS = % e Ts = s 
-const float kp_z = 5.86;
-const float kd_z = 3.42;
+//%OS = 0.5% e Ts = 2s 
+const float kp_z = 5.40;
+const float kd_z = 4.00;
 
 //Parâmetros do estimador horizontal
-const float sigma = 2*tan(42/2)/(420*dt);
+const float sigma = 2*tan((42*pi/180)/2)/(420*dt);
 const float wc_hz = 50; //frequência de corte do estimador horizontal (rad/s)
-const float l_hz = 1; //ganho do estimador horizontal (observador de estados)
+const float l_hz = wc_hz; //ganho do estimador horizontal (observador de estados)
 
 
 #endif
